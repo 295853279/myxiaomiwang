1dcc04205667ba719dfa625ab0c2795941dfbcd0
295853279 <295853279@qq.com>
2018-04-06 15:11:01 +0800

    完成小米开发了

---
+++ js/index.js
@@ -0,0 +1,82 @@
+
+// var a=""
+// for (var i=1;i<=9;i++){
+//     for (var j=1;j<=i;j++){
+//         a += j+"*"+i+"="+i*j;
+//         a+="  "
+//     }
+//         a+="\n"
+// }
+// console.log(a)
+
+// var b=""
+// for(var i=1;i<5;i++){
+//     for(var j=1;j<=i;j++){
+//         b=b+"*"
+//         b+=" "
+//     }
+//     b+="\n"
+// }
+// console.log(b)
+
+// var c=""
+// for(var i=1;i<5;i++){
+//     for(var j=1;j<=4;j++){
+//         c=c+"*"
+//         c+=" "
+//     }
+//     c+="\n"
+// }
+// console.log(c)
+
+// var d=""
+// for(var i=1;i<=7;i++){
+//     for(var j=1;j<=i;j++){
+//         if(i == 4)
+//         continue;
+//         if(i == 6)
+//         continue;
+//         d=d+"*"
+//         d+=" "
+//     }
+//     d+="\n"
+// }
+//
+// console.log(d)
+
+// for(var i=10;i>0;i--){
+//     for(var j=10;j>0;j--){
+//         if(j<=i){
+//            document.write("☆")
+//         }else{
+//             document.write("&nbsp;")
+//         }
+//     }
+//     document.write("<br/>")
+// }
+
+//
+// for(var i=7;i>0;i--){
+//     for(var j=1;j<=7;j++){
+//         if(j>=i){
+//             document.write("☆")
+//         }else{
+//             document.write("&nbsp;")
+//         }
+//     }
+//     document.write("<br/>")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
